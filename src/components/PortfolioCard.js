a63c18a54e318cad92d79c2102513abd22acfc7f
Zach <zawood11@gmail.com>
2022-03-23 01:10:37 -0400

    portfolio card link

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const PortfolioCard = ({ portfolio }) => {
+  const {id} = useParams()
+  const [portfolioObj, setPortfolioObj] = useState(null);
+
+  useEffect(() => {   
+    if (!portfolio) {
+        fetch(`http://localhost:9292/portfolios/${id}`)
+        .then(res => res.json())
+        .then(portfolio => setPortfolioObj(portfolio))
+        }
+    }, [portfolio, id]);
+
+    const finalPortfolio = portfolio ? portfolio : portfolioObj
+    if (!finalPortfolio) return <h1>Loading...</h1>
   return (
     <div>
-        <h3>{portfolio.name}</h3>
+        <h3><Link to={`/portfolios/${finalPortfolio.id}`}>{finalPortfolio.name}</Link></h3>
     </div>
   )
 }
